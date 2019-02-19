f0ada948ebada0ebf9ebea38883998e8492b51db
Yu Brian Yao <yyao@sv870160lx.am.mds.honda.com>
2019-02-19 14:24:41 -0800

    ready to use

--- run_inference.sh
+++ run_inference.sh
@@ -1,3 +1,15 @@
+#export CUDA_VISIBLE_DEVICES=0
+#python main.py \
+#    --skip_validation \
+#    --skip_train \
+#    --inference \
+#    --no_loss \
+#    --save_flow \
+#    --model FlowNet2 \
+#    --inference_dataset TaiwanSA \
+#    --inference_dataset_root data/taiwan_sa/testing \
+#    --inference_size 320 192 \
+#    --resume checkpoints/FlowNet2_checkpoint.pth.tar
 export CUDA_VISIBLE_DEVICES=0
 python main.py \
     --skip_validation \
