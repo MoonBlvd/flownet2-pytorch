4bef4741d2dcd01e37a64a63d6c39798c64ebcb7
BrianYao <brianyao@Xinmings-MacBook-Pro.local>
2019-02-19 23:39:45 -0500

    mac

--- run_inference.sh
+++ run_inference.sh
@@ -1,3 +1,15 @@
+#export CUDA_VISIBLE_DEVICES=0
+#python main.py \
+#    --skip_validation \
+#    --skip_train \
+#    --inference \
+#    --no_loss \
+#    --save_flow \
+#    --model FlowNet2 \
+#    --inference_dataset TaiwanSA \
+#    --inference_dataset_root data/taiwan_sa/testing \
+#    --inference_size 320 192 \
+#    --resume checkpoints/FlowNet2_checkpoint.pth.tar
 export CUDA_VISIBLE_DEVICES=0
 python main.py \
     --skip_validation \
@@ -7,6 +19,6 @@ python main.py \
     --save_flow \
     --model FlowNet2 \
     --inference_dataset TaiwanSA \
-    --inference_dataset_root data/taiwan_sa/testing \
+    --inference_dataset_root /data/A3D/frames \
     --inference_size 320 192 \
     --resume checkpoints/FlowNet2_checkpoint.pth.tar
